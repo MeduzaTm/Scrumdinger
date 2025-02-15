14f4945e8aed60631fdea3e5a917a4a5c5cb3a2b
Nurik <nurik080603@gmail.com>
2025-02-15 13:07:07 +0300

    Second Commit

--- Scrumdingerr/Models/ScrumStore.swift
+++ Scrumdingerr/Models/ScrumStore.swift
@@ -12,12 +12,15 @@ class ScrumStore: ObservableObject {
     @Published var scrums: [DailyScrum] = []
     
     private static func fileURL() throws -> URL {
-        try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-            .appendingPathComponent("scrum.data")
+        try FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+        .appendingPathComponent("scrums.data")
     }
     
     func load() async throws {
-        let task = Task<[DailyScrum],Error> {
+        let task = Task<[DailyScrum], Error> {
             let fileURL = try Self.fileURL()
             guard let data = try? Data(contentsOf: fileURL) else {
                 return []
@@ -31,8 +34,8 @@ class ScrumStore: ObservableObject {
     
     func save(scrums: [DailyScrum]) async throws {
         let task = Task {
-            let data = try! JSONEncoder().encode(scrums)
-            let outfile = try! Self.fileURL()
+            let data = try JSONEncoder().encode(scrums)
+            let outfile = try Self.fileURL()
             try data.write(to: outfile)
         }
         _ = try await task.value
---
+++ Scrumdingerr/Views/ErrorView.swift
@@ -0,0 +1,51 @@
+//
+//  ErrorView.swift
+//  Scrumdingerr
+//
+//  Created by Нурик  Генджалиев   on 15.02.2025.
+//
+
+import SwiftUI
+
+
+struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        VStack {
+            Text("An error has occured!")
+                .font(.title)
+                .padding(.bottom)
+            Text(errorWrapper.error.localizedDescription)
+                .font(.headline)
+            Text(errorWrapper.guidance)
+                .font(.caption)
+                .padding(.top)
+            Spacer()
+        }
+        .padding()
+        .background(.ultraThinMaterial)
+        .cornerRadius(16)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Dismiss") {
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+
+struct ErrorView_Previews: PreviewProvider {
+    enum SampleError: Error {
+        case errorRequired
+    }
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired, guidance: "You can safely ignore this error.")
+    }
+    static var previews: some View {
+        ErrorView(errorWrapper: wrapper)
+    }
+}
